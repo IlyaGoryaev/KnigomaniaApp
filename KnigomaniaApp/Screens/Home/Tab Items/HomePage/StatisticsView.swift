c9bbefa0074b5b6de039887405a29fab94de3619
Arina <arinapostnikova123@gmail.com>
2023-10-22 12:57:03 +0300

    fix StatisticsView

--- KnigomaniaApp/Screens/Home/Tab Items/HomePage/StatisticsView.swift
+++ KnigomaniaApp/Screens/Home/Tab Items/HomePage/StatisticsView.swift
@@ -8,36 +8,31 @@
 import SwiftUI
 
 struct StatisticsView: View {
-	
-    @State private var selection = 0
-	
-	@State private var friends: [FriendModel] = [FriendModel(friendName: "ejfj", goalValue: 10, currentValue: 4), FriendModel(friendName: "dwqdwq", goalValue: 8, currentValue: 3), FriendModel(friendName: "ghiohgior", goalValue: 12, currentValue: 7), FriendModel(friendName: "fhioeshif", goalValue: 34, currentValue: 12)]
-	
-	weak var mainScreenCoordinator: MainCoordinator?
     
-    init() {
-        UIPageControl.appearance().currentPageIndicatorTintColor = UIColor(CustomColors.darkBrownColor)
-        UIPageControl.appearance().pageIndicatorTintColor = UIColor(CustomColors.brownColor.opacity(0.75))
-    }
+    @State private var selection = 0
+    
+    @State private var friends: [FriendModel] = [FriendModel(friendName: "ejfj", goalValue: 10, currentValue: 4), FriendModel(friendName: "dwqdwq", goalValue: 8, currentValue: 3), FriendModel(friendName: "ghiohgior", goalValue: 12, currentValue: 7), FriendModel(friendName: "fhioeshif", goalValue: 34, currentValue: 12)]
+    
+    weak var mainScreenCoordinator: MainCoordinator?
     
     var body: some View {
         TabView(selection: $selection) {
             MyProgress(isTracketSetup: true, goalValue: 15, currentValue: 10, mainScreenCoordinator: mainScreenCoordinator)
-				.onAppear(perform: {
-					DispatchQueue.main.async {
-						if !friends.isEmpty{
-							for i in 0...friends.count - 1 {
-								friends[i].offset = 0
-							}
-						}
-					}
-				})
+                .onAppear(perform: {
+                    DispatchQueue.main.async {
+                        if !friends.isEmpty{
+                            for i in 0...friends.count - 1 {
+                                friends[i].offset = 0
+                            }
+                        }
+                    }
+                })
                 .tag(0)
             FriendsStatisticView(friendsArray: $friends)
                 .tag(10)
         }
         .tabViewStyle(.page)
-		.ignoresSafeArea()
+        .ignoresSafeArea()
     }
 }
 
